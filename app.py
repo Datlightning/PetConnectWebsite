27b82f7ace4a70c42ab65bfee29da9b17dcb15fc
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2023-10-31 07:30:56 -0400

    Fonyt + rendererr

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+@app.route("/products")
+def productsPage(): 
+    return render_template("products.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
