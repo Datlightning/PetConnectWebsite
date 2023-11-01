9b78dd45f2871f2c19872df6b819af304c327a50
Datlightning <79178850+Datlightning@users.noreply.github.com>
2023-11-01 08:00:34 -0400

    added templatee

--- app.py
+++ app.py
@@ -10,7 +10,30 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    products = [
+        "THIS IS A TEMPLATE FOR A PRODUCT BECAUSE I AM REALLY LAZY",
+        "THIS IS A TEMPLATE FOR A PRODUCT BECAUSE I AM REALLY LAZY",
+        "THIS IS A TEMPLATE FOR A PRODUCT BECAUSE I AM REALLY LAZY"
+    ]
+    description = [
+        "MIGHT BE THE DUMBEST PRODUCT I HAVE EVER SEEN (RUTHVIK SAID THIS)",
+        "the big BOX",
+        "CHAT IS THIS REAL"
+    ]
+    names = [
+        "PET UMBRELLA",
+        "PET BOX",
+        "PET SHOES"
+    ]
+    pictures = [
+        "../static/images/petconnect_logo.png",
+        "../static/images/petconnect_logo.png",
+        "../static/images/petconnect_logo.png",
+        "../static/images/petconnect_logo.png"
+    ]
+
+
+    return render_template("index.html", products = products, names = names, descriptions = description, pictures =pictures)
 
 if __name__ == '__main__':
     app.run(debug=True)
