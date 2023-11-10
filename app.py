e460c4017ff2865b5a05638dc536a557e7a56821
Datlightning <79178850+Datlightning@users.noreply.github.com>
2023-11-10 15:04:03 -0500

    Added Product Line Ups and Animations so it doesnt look trash

--- app.py
+++ app.py
@@ -34,30 +34,18 @@ def index():
     return render_template("index.html",urls = data["urls"], names = data["names"], pictures = data["pictures"], descriptions = data['descriptions'])
 @app.route("/petumbrella")
 def umbrella():
-    data = rd.getProducts()
-    index = 0
-    for i in data["names"]:
-        if "umbrella" in i.lower():
-            break
-        index += 1
-    return render_template("product.html", name = data["names"][index])
+    data = rd.getProduct("umbrella")
+  
+    return render_template("product.html", products = data["product-names"], name = data["name"], pictures = data["product-pictures"], description = data['product-descriptions'])
 @app.route("/petshoes")
 def shoes():
-    data = rd.getProducts()
-    index = 0
-    for i in data["names"]:
-        if "shoe" in i.lower():
-            break
-        index += 1
-    return render_template("product.html", name = data["names"][index])
+    data = rd.getProduct("shoe")
+  
+    return render_template("product.html", products = data["product-names"], name = data["name"], pictures = data["product-pictures"], description = data['product-descriptions'])
 @app.route("/petfeeder")
 def feeder():
-    data = rd.getProducts()
-    index = 0
-    for i in data["names"]:
-        if "feed" in i.lower():
-            break
-        index += 1
-    return render_template("product.html", name = data["names"][index])
+    data = rd.getProduct("feed")
+  
+    return render_template("product.html", products = data["product-names"], name = data["name"], pictures = data["product-pictures"], description = data['product-descriptions'])
 if __name__ == '__main__':
     app.run(debug=True)
