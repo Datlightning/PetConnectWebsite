0a3affc559370b079cb9d660e5cdb1097459d009
Ruthvik Venkatesan <65773092+Cylinders@users.noreply.github.com>
2023-12-06 08:37:47 -0500

    LexicalParser.py + blogpost.py
    Custom Txt blog post system

--- static/images/accessfolders.py
+++ static/images/accessfolders.py
@@ -14,3 +14,15 @@ def update():
     except:
         pass
     shutil.move(source.resolve(), destination.resolve())
+def updateImagesBlog(): 
+    directory = Path(__file__).parent.parent.parent
+    source = directory.joinpath("products")
+    destination = directory.joinpath("static").joinpath("images")
+    expected = directory.joinpath("static").joinpath("images").joinpath('products')
+    download_url = "https://drive.google.com/drive/folders/1NyULx6fbRsH7Qpj3prsu4LDtQn_9CSFN"
+    gdown.download_folder(url=download_url, quiet = True, use_cookies=True)
+    try:
+        shutil.rmtree(expected.resolve())
+    except:
+        pass
+    shutil.move(source.resolve(), destination.resolve())
